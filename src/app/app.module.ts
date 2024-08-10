4b377fc8e0a07b7cdfb457b2b41f0824430b9fe3
wendel dos santos nunes <wendelnunes9999@gmail.com>
2024-08-10 09:26:20 -0300

    :sparkles: feat: Home page and header component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderHomePageComponent } from './shared/components/header-home-page/header-home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderHomePageComponent
   ],
   imports: [
     BrowserModule,
