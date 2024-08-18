a291dd0c9599d442046f0c6893e54f29f5cd7a8d
wendel dos santos nunes <wendelnunes9999@gmail.com>
2024-08-17 21:18:24 -0300

    :sparkles: feat: update environment variable

--- mynode.js
+++ mynode.js
@@ -11,13 +11,13 @@ const envFile = `export const environment = {
     YOUR_PUBLIC_KEY: '${process.env.YOUR_PUBLIC_KEY}',
   };
 `;
-const targetPath = path.join(__dirname, './src/environments/environment.ts');
+const targetPath = path.join(__dirname, './src/environments/environment.prod.ts');
 
 fs.writeFile(targetPath, envFile, (err) => {
   if (err) {
     console.error(err);
     throw err;
   } else {
-    console.log(successColor, `${checkSign} Successfully generated environment.ts`);
+    console.log(successColor, `${checkSign} Successfully generated environment.prod.ts`);
   }
 });
