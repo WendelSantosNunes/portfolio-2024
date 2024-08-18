abcf9be0557eba63d92069ac509eb7808b5ca5f4
wendel dos santos nunes <wendelnunes9999@gmail.com>
2024-08-18 13:38:18 -0300

    :ok_hand: style: An anchor was added to the a tag and an animation was added to the site

--- src/app/shared/components/header-home-page/header-home-page.component.ts
+++ src/app/shared/components/header-home-page/header-home-page.component.ts
@@ -7,15 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderHomePageComponent implements OnInit {
 
-  isDark = false
+  isDark!: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.isDark = localStorage.getItem('isDark') === 'true';
+    document.body.classList.toggle('dark', this.isDark)
   }
 
   toggleTheme(){
     this.isDark = !this.isDark;
     document.body.classList.toggle('dark', this.isDark)
+    localStorage.setItem("isDark", this.isDark.toString())
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,6 @@
-import { Environment } from "src/app/core/utils/environment-interface";
-
-export const environment: Environment = {
-  production: true,
-  YOUR_SERVICE_ID: '',
-  YOUR_TEMPLATE_ID: '',
-  YOUR_PUBLIC_KEY: '',
-};
+export const environment = {
+    production: false,
+    YOUR_SERVICE_ID: '',
+    YOUR_TEMPLATE_ID: '',
+    YOUR_PUBLIC_KEY: '',
+  };
